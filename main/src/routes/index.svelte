ab4af89d32a1ee07e180c4ca967ef0d88d3e612c
jgero <johannes.gerold.ext@gmail.com>
2020-11-26 23:42:07 +0100

    added main page

---
+++ main/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <title>placeholder</title>
+</svelte:head>
+
+<h1>placeholder</h1>
+
+<p>
+  This page is currently just a placeholder. The portfolio can be found
+  <a href="https://portfolio.jgero.me">here</a>
+  and the sapce stats page
+  <a href="https://space.jgero.me">here</a>
+</p>
