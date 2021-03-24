f2e7ba9198e8a007f7848b056e862235a54f6b91
Johannes Gerold <33359660+jgero@users.noreply.github.com>
2021-03-24 11:38:34 +0100

    text at location test (#66)

--- scavenger-hunt/src/routes/ar/demo.svelte
+++ scavenger-hunt/src/routes/ar/demo.svelte
@@ -1,21 +1,35 @@
 <script>
-	import { onMount } from "svelte";
-	onMount(() => {
-			console.log("I am alive!");
-			// 48.937930742030865, 10.209591656625395
-		setInterval(() => {
-			const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-			console.log("measuring: ");
-			console.log(distanceMsg);
-		}, 3000);
-	});
-	// <a-box material="color: yellow" gps-entity-place="latitude: <your-latitude>; longitude: <your-longitude>"/>
+  import { onMount } from "svelte";
+  let distanceText = "distance: not initialized";
+  onMount(() => {
+    console.log("I am alive!");
+    setInterval(() => {
+      const distanceMsg = document
+        .querySelector("[gps-entity-place]")
+        .getAttribute("distanceMsg");
+      distanceText = `distance: ${distanceMsg}`;
+    }, 3000);
+  });
 </script>
 
+<style>
+  h1 {
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+</style>
+
+<h1>{distanceText}</h1>
+
 <a-scene
-	vr-mode-ui="enabled: true"
-	embedded
-	arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: true;">
-	<a-camera gps-camera rotation-reader />
-	<a-box material="color: yellow"  gps-entity-place="latitude: 48.938411863997246; longitude: 10.209873807386952"/>
+  vr-mode-ui="enabled: false"
+  embedded
+  arjs="sourceType: webcam; debugUIEnabled: false;">
+  <a-text
+    value="This content will always face you."
+    look-at="[gps-camera]"
+    scale="120 120 120"
+    gps-entity-place="latitude: 48.938411863997246; longitude: 10.209873807386952;" />
+  <a-camera gps-camera rotation-reader />
 </a-scene>
