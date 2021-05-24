fe79e4d1f493d613b991897d28cd0348275f2df9
jgero <33359660+jgero@users.noreply.github.com>
2021-05-24 16:30:33 +0200

    started working on create route editor
    - better list display of places
    - better display of current searchers

--- scavenger-hunt/src/routes/create/index.svelte
+++ scavenger-hunt/src/routes/create/index.svelte
@@ -147,6 +147,29 @@
 <RouteHeader title="Route bearbeiten" />
 
 <main>
+{#if route && route.places}
+    <h2>Stationen:</h2>
+    <ul>
+        {#each route.places as place}
+            <li on:click={() => (selectedPlace = place)} class:selected={selectedPlace && selectedPlace.id ===
+                    place.id}>
+                {place.name}
+                <button on:click={() => (selectedPlace = place)}><span
+                        class="material-icons">edit</span></button>
+            </li>
+        {/each}
+        <li><button class="align-left"  on:click={setupNewLocation}><span class="material-icons">add</span></button></li>
+    </ul>
+
+    <section>
+        <div>
+    <h2>Teilnehmer: {route.searchers ? route.searchers.length : 0}</h2>
+    <button on:click={resetSearchers}><span class="material-icons">clear</span> RESET</button>
+            </div>
+        <p>Scanne den QR-Code von den Mitspielern um sie deiner Route hinzuzufügen.</p>
+        </section>
+{/if}
+
     {#if selectedPlace}
         <form on:submit|preventDefault={savePlace} bind:this={form}>
             <input
@@ -175,6 +198,10 @@
 
             <ImageCapture bind:imageDataUrl={newImageDataUrl} />
             <button type="submit">SAVE</button>
+            <button type="button" on:click={() => ( selectedPlace = null )}>CLOSE</button>
+                <button type="button" on:click={() => deletePlace(selectedPlace)}
+                    >DELETE</button
+                >
             {#if $myCoords}
                 <button type="button" on:click={useCurrentLocation}
                     >USE MY LOCATION</button
@@ -184,31 +211,60 @@
     {/if}
 </main>
 
-<button on:click={setupNewLocation}>NEW</button>
-
-{#if route && route.places}
-    <ul>
-        {#each route.places as place}
-            <li on:click={() => (selectedPlace = place)}>
-                {place.name}
-                <button on:click={() => deletePlace(place)}>DELETE</button>
-            </li>
-        {/each}
-    </ul>
-
-    <p>{route.searchers ? route.searchers.length : 0} suchende sind der Route hinzugefügt</p>
-    <button on:click={resetSearchers}>suchende löschen</button>
-{/if}
-
 <style>
+    main {
+    padding: 1rem;
+}
+ul {
+    margin: 0;
+    padding: 0;
+}
+
+section div {
+display: flex;
+align-items: center;
+justify-content: space-between;
+}
+section div button {
+    height: min-content;
+}
+    li {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    padding-left: 1.5rem;
+}
+ 
+button.align-left {
+margin-left: 0;
+}
     form {
         display: grid;
         grid-template-rows: 2fr 1fr 1fr;
         grid-template-columns: 25% 75%;
         grid-template-areas: 'h h' 'latn latc' 'lonn lonc';
+        border-radius: 0.6rem;
+        box-shadow: -2px -2px 4px 0px #ffffff, 2px 2px 4px 0px #00000025;
+    padding: 1rem;
     }
     form #name {
         font-size: 2rem;
         grid-area: h;
+    }
+button {
+    background-color: var(--primary);
+    border-radius: 6px;
+    color: white;
+    text-decoration: none;
+    display: inline-flex;
+    align-items: center;
+    width: max-content;
+    padding: 0.2rem 1rem;
+    margin: 0.5rem;
+    border: none;
+    outline: none;
+}
+    button span {
+        margin-right: 0.2rem;
     }
 </style>
