59ec97bccc3ebdca42cc1f9648bdca66afe63a2e
jgero <33359660+jgero@users.noreply.github.com>
2021-07-09 11:35:52 +0200

    fixed formatting

--- scavenger-hunt/src/components/LocationDetails.svelte
+++ scavenger-hunt/src/components/LocationDetails.svelte
@@ -1,6 +1,6 @@
 <script>
     import firebase from 'firebase/app';
-    import { onMount} from "svelte";
+    import { onMount } from 'svelte';
     import { getLogger } from '../stores/debug-logger';
 
     export let location, routeId;
@@ -9,13 +9,16 @@
     onMount(async () => {
         logger = getLogger();
         try {
-            imageUrl = await firebase.storage().ref()
-                            .child(`${routeId}/${location.id}`).getDownloadURL();
+            imageUrl = await firebase
+                .storage()
+                .ref()
+                .child(`${routeId}/${location.id}`)
+                .getDownloadURL();
         } catch (e) {
             logger.log({
-            logLevel: "error",
-            message: `could not load image for location ${location.id}`
-            }) 
+                logLevel: 'error',
+                message: `could not load image for location ${location.id}`,
+            });
         }
     });
 </script>
@@ -26,13 +29,12 @@
     <img src={imageUrl} alt={location.name} />
 </section>
 
-
 <style>
-section {
-    overflow: auto;
-    margin: 2rem 0;
-}
-img {
-    max-width: 80vw;
-}
+    section {
+        overflow: auto;
+        margin: 2rem 0;
+    }
+    img {
+        max-width: 80vw;
+    }
 </style>
--- scavenger-hunt/src/routes/create/addUser/[newSearcher].svelte
+++ scavenger-hunt/src/routes/create/addUser/[newSearcher].svelte
@@ -5,55 +5,62 @@
 </script>
 
 <script>
-import firebase from 'firebase/app';
-import { onDestroy, onMount } from 'svelte';
-import { getUserId } from "../../../stores/user.js";
+    import firebase from 'firebase/app';
+    import { onDestroy, onMount } from 'svelte';
+    import { getUserId } from '../../../stores/user.js';
 
-export let newSearcher;
+    export let newSearcher;
 
-let userId;
-let isLoading = true;
+    let userId;
+    let isLoading = true;
 
-let unsubUser;
+    let unsubUser;
 
-onMount(() => {
-    userId = getUserId();
-    unsubUser = userId.subscribe(addUser);
-});
+    onMount(() => {
+        userId = getUserId();
+        unsubUser = userId.subscribe(addUser);
+    });
 
-onDestroy(() => {
-    if (unsubUser) {
-        unsubUser();
+    onDestroy(() => {
+        if (unsubUser) {
+            unsubUser();
+        }
+    });
+
+    async function addUser(routeOwnerId) {
+        if (!newSearcher || !routeOwnerId) {
+            return;
+        }
+        // get the route
+        const doc = await firebase
+            .firestore()
+            .collection('routes')
+            .doc(routeOwnerId)
+            .get();
+        let route = doc.data();
+        // add the new searcher
+        if (route.searchers && Array.isArray(route.searchers)) {
+            route.searchers.push(newSearcher);
+        } else {
+            route.searchers = [newSearcher];
+        }
+        // update the route
+        await firebase
+            .firestore()
+            .collection('routes')
+            .doc(routeOwnerId)
+            .set(route);
+        isLoading = false;
     }
-});
-
-async function addUser(routeOwnerId) {
-    if (!newSearcher || !routeOwnerId) {
-        return;
-    }
-    // get the route
-    const doc = await firebase.firestore().collection("routes").doc(routeOwnerId).get();
-    let route = doc.data();
-    // add the new searcher
-    if (route.searchers && Array.isArray( route.searchers )) {
-        route.searchers.push(newSearcher);
-    } else {
-        route.searchers = [newSearcher];
-    }
-    // update the route
-    await firebase.firestore().collection("routes").doc(routeOwnerId).set(route);
-    isLoading = false;
-}
 </script>
 
 {#if isLoading}
-{#if !$userId || !newSearcher}
-   <h1>loading...</h1>
-{:else}
-    <h1>Adding user {$userId} to route</h1>
-{/if}
-
+    {#if !$userId || !newSearcher}
+        <h1>loading...</h1>
+    {:else}
+        <h1>Adding user {$userId} to route</h1>
+    {/if}
 {:else}
     <h1>User {$userId} added to route</h1>
-    <a href="/create">OK</a> 
+    <a href="/create">OK</a>
 {/if}
