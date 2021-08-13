8b8209739f84c37a0d573b8c9a084260577b79ad
Johannes Gerold <33359660+jgero@users.noreply.github.com>
2021-08-13 16:20:58 +0200

    Join other routes (#11)
    * translated text to german on add person page
    
    * join page ui rework and list with routes to join

--- scavenger-hunt/src/routes/join/index.svelte
+++ scavenger-hunt/src/routes/join/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import firebase from 'firebase/app';
 	import { onDestroy, onMount } from 'svelte';
 	import QRCode from 'qrcode';
 
@@ -9,6 +10,7 @@
 	let userId, logger;
 	let qrBase64;
 	let unsub;
+	let routesToJoin = [], unsubRoutesToJoin;
 	onMount(() => {
 		userId = getUserId();
 		logger = getLogger();
@@ -22,7 +24,27 @@
 				hostname === 'localhost' ? 'http://localhost' : `https://${hostname}`;
 			addMeURL += `/create/addUser/${uid}`;
 			writeQrCode(addMeURL);
+		unsubRoutesToJoin = firebase
+			.firestore()
+			.collection('routes').where("searchers", "array-contains", uid)
+			.onSnapshot(
+				(snapshot) => {
+					routesToJoin = [];
+					snapshot.forEach(doc => routesToJoin.push(doc.id));
+					logger.log({
+						logLevel: 'log',
+						message: `routes to join for user ${uid} loaded`,
+					});
+				},
+				(error) => {
+					logger.log({
+						logLevel: 'error',
+						message: `could not fetch route data: ${JSON.stringify(error)}`,
+					});
+				}
+			);
 		});
+
 	});
 
 	function writeQrCode(dataString) {
@@ -46,15 +68,68 @@
 		if (unsub) {
 			unsub();
 		}
+		if(unsubRoutesToJoin)  {
+unsubRoutesToJoin();
+	}
 	});
 </script>
 
 <RouteHeader title="Route beitreten" />
-{#if $userId}
-	<h1>Meiner eigenen Route beitreten</h1>
-	<a href="/join/{$userId}">beitreten</a>
-{/if}
 
-<h2>dein Freund muss diesen code scannen:</h2>
+<main>
+	<h2>Routen</h2>
+<ul>
+{#if $userId}
+	<li>
+	<a href="/join/{$userId}">Meiner eigenen Route beitreten</a>
+			</li>
+{/if}
+{#each routesToJoin as route}
+	<li>
+	<a href="/join/{route}">Route von Benutzer {route}</a>
+		</li>
+{/each}
+</ul>
+
+<h2>Code um mich zu einer Route einzuladen</h2>
 
 <img src={qrBase64} alt="Einladungs QR-Code" />
+</main>
+
+<style>
+	main {
+		padding: 1rem;
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+		justify-items: flex-start;
+	}
+	ul {
+		margin: 0;
+		padding: 0;
+		max-width: 100%;
+	}
+	li {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-block-end: 1rem;
+		padding-left: 1.5rem;
+		padding-inline-start: 4rem;
+	}
+	img {
+		max-width: 50%;
+		align-self: center;
+	}
+	a {
+		background-color: var(--primary);
+		border-radius: 0.6rem;
+		color: var(--primary-contrast);
+		text-decoration: none;
+		padding: 0.5rem;
+		display: block;
+		width: max-content;
+	}
+</style>
+
+
